2651ccfdfc14caa48fc2351ba17d3ebe13bc22fd
sr54loso <sr54loso@studserv.uni-leipzig.de>
2020-05-15 09:26:06 +0200

    Added test for getOutgoingEdges method

--- modules/indexmodule/src/test/java/de/peyrer/graph/JGraphTAdapterTest.java
+++ modules/indexmodule/src/test/java/de/peyrer/graph/JGraphTAdapterTest.java
@@ -3,6 +3,7 @@ package de.peyrer.graph;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Iterator;
 import java.util.Map;
 
 public class JGraphTAdapterTest {
@@ -21,5 +22,25 @@ public class JGraphTAdapterTest {
         }
 
         Assert.assertTrue(scores.size() > 1);
+    }
+
+    @Test
+    public void testGetOutgoingEdges()
+    {
+        JGraphTAdapter jGraphTAdapter = new JGraphTAdapter();
+
+        jGraphTAdapter.addVertex("1");
+        jGraphTAdapter.addVertex("2");
+        jGraphTAdapter.addVertex("3");
+        jGraphTAdapter.addEdge("1", "2", "1");
+        jGraphTAdapter.addEdge("3", "1", "1");
+
+        Iterable<String[]> resultIterable = jGraphTAdapter.getOutgoingEdges("1");
+
+        Assert.assertTrue(resultIterable.iterator().hasNext());
+
+        for(String[] result : resultIterable){
+            Assert.assertArrayEquals(result, new String[]{"1", "2", "1"});
+        }
     }
 }
