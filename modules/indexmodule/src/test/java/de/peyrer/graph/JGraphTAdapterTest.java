21fe37f996f80fa278eb9ac3ea953b89f2c21f30
sr54loso <sr54loso@studserv.uni-leipzig.de>
2020-05-17 14:02:07 +0200

    Adapted jgraphtadapter test

--- modules/indexmodule/src/test/java/de/peyrer/graph/JGraphTAdapterTest.java
+++ modules/indexmodule/src/test/java/de/peyrer/graph/JGraphTAdapterTest.java
@@ -1,21 +1,43 @@
 package de.peyrer.graph;
 
+import de.peyrer.repository.ArgumentRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Iterator;
 import java.util.Map;
 
 public class JGraphTAdapterTest {
 
+    @Mock (name = "argumentRepository")
+    ArgumentRepository argumentRepository;
+
+    @InjectMocks
+    JGraphTAdapter jGraphTAdapter;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+        jGraphTAdapter.addVertex("1");
+        jGraphTAdapter.addVertex("2");
+        jGraphTAdapter.addVertex("3");
+        jGraphTAdapter.addEdge("1", "2", "1");
+        jGraphTAdapter.addEdge("3", "1", "1");
+    }
+
     @Test
     public void testComputePageRank()
     {
-        GraphBuilder builder = new GraphBuilder(GraphBuilder.GraphType.JGRAPHT, GraphBuilder.MatcherType.AND);
+        Mockito.when(argumentRepository.updatePageRank(Mockito.anyString(), Mockito.anyDouble()))
+                .thenAnswer(i -> i.getArgument(1));
 
-        IDirectedGraph graph = builder.build();
-
-        Map<String, Double> scores = graph.computeAndSavePageRank();
+        Map<String, Double> scores = jGraphTAdapter.computeAndSavePageRank();
 
         for(Map.Entry<String,Double> entry : scores.entrySet()){
             Assert.assertTrue(entry.getValue() > 0);
@@ -27,14 +49,6 @@ public class JGraphTAdapterTest {
     @Test
     public void testGetOutgoingEdges()
     {
-        JGraphTAdapter jGraphTAdapter = new JGraphTAdapter();
-
-        jGraphTAdapter.addVertex("1");
-        jGraphTAdapter.addVertex("2");
-        jGraphTAdapter.addVertex("3");
-        jGraphTAdapter.addEdge("1", "2", "1");
-        jGraphTAdapter.addEdge("3", "1", "1");
-
         Iterable<String[]> resultIterable = jGraphTAdapter.getOutgoingEdges("1");
 
         Assert.assertTrue(resultIterable.iterator().hasNext());
