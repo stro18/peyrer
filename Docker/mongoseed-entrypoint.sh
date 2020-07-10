eeb9358a0d5443dbb662c7101c96742f606aaf40
Stefan Rosenlund <sr54loso@studserv.uni-leipzig.de>
2020-07-10 12:45:53 +0200

    Fixed typo in file name

--- Docker/mongoseed-entrypoint.sh
+++ Docker/mongoseed-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debateorg.ndjson.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
+mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debateorg.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
 mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debatepedia.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
 mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debatewise.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
 mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/idebate.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
