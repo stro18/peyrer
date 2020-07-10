63436af27f5861664d14be3241d52d488efac8be
Stefan Rosenlund <sr54loso@studserv.uni-leipzig.de>
2020-07-10 12:43:18 +0200

    Fixed entrypoint.sh

--- Docker/mongoseed-entrypoint.sh
+++ Docker/mongoseed-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CMD mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debateorg.ndjson.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
-CMD mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debatepedia.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
-CMD mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debatewise.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
-CMD mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/idebate.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
-CMD mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/parliamentary.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
+mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debateorg.ndjson.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
+mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debatepedia.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
+mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/debatewise.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
+mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/idebate.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
+mongoimport --host mongo --db peyrer --collection args --file /argument_ndjson/parliamentary.ndjson --username root --password example --authenticationDatabase admin --mode=upsert  --upsertFields=id
