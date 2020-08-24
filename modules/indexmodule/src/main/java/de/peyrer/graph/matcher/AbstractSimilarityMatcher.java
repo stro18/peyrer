825aa88f230ae65f60448b0d6242ff1e1c6e5c54
Stefan Rosenlund <sr54loso@studserv.uni-leipzig.de>
2020-08-24 17:52:37 +0200

    Completed method searchPremiseIndex

--- modules/indexmodule/src/main/java/de/peyrer/graph/matcher/AbstractSimilarityMatcher.java
+++ modules/indexmodule/src/main/java/de/peyrer/graph/matcher/AbstractSimilarityMatcher.java
@@ -1,5 +1,6 @@
 package de.peyrer.graph.matcher;
 
+import org.apache.lucene.document.Document;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
@@ -9,14 +10,24 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-public abstract class AbstractSimilarityMatcher implements ISimilarityMatcher {
-
+public abstract class AbstractSimilarityMatcher implements ISimilarityMatcher
+{
     protected Iterable<Map<String,String>> searchPremiseIndex(IndexSearcher searcher, Query query, int limit, int threshold) throws IOException {
+        LinkedList<Map<String,String>> result = new LinkedList<>();
+
         ScoreDoc[] hits = searcher.search(query, limit).scoreDocs;
 
-        LinkedList<Map<String,String>> result = new LinkedList<>();
-        Map<String,String> first = new HashMap<>();
-        result.add(first);
+        for (int i = 0; i < hits.length; i++) {
+            if (threshold != 0 && hits[i].score < threshold){
+                break;
+            }
+
+            Document doc = searcher.doc(hits[i].doc);
+            Map<String,String> found = new HashMap<>();
+            found.put("argumentId", doc.get("argumentId"));
+            found.put("score", String.valueOf(hits[i].score));
+            result.add(found);
+        }
 
         return result;
     }
