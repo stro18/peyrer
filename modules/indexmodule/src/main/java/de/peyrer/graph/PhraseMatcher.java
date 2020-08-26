d47e3c560c4d5962067fe3aefa21524b12ade092
Stefan Rosenlund <sr54loso@studserv.uni-leipzig.de>
2020-08-26 14:11:33 +0200

    Fixed nullpointer exception in Phrase and PhrasePremise matcher

--- modules/indexmodule/src/main/java/de/peyrer/graph/PhraseMatcher.java
+++ modules/indexmodule/src/main/java/de/peyrer/graph/PhraseMatcher.java
@@ -17,6 +17,7 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class PhraseMatcher extends AbstractMatcher {
 
@@ -27,31 +28,39 @@ public class PhraseMatcher extends AbstractMatcher {
     @Override
     public Iterable<Map<String,String>> match() throws IOException
     {
-        LinkedList<Map<String,String>> result = new LinkedList<>();
         Directory directory = FSDirectory.open(Paths.get(directoryName));
         DirectoryReader iReader = DirectoryReader.open(directory);
         this.searcher = new IndexSearcher(iReader);
-        Analyzer analyzer = (new AnalyzerModule()).getAnalyzer();
+
+        AnalyzerModule analyzerModule = new AnalyzerModule();
+        Analyzer analyzer = analyzerModule.getAnalyzer();
         this.parser = new QueryParser("premiseText", analyzer);
-        Query query = this.parser.createPhraseQuery("premiseText", new AnalyzerModule().analyze("premiseText",stringToMatch));
 
-        int matchNumber = this.searcher.count(query);
-        if(matchNumber == 0) {
-            return result;
-        }
-        ScoreDoc[] hits = this.searcher.search(query, matchNumber).scoreDocs;
+        int wordsCount = new StringTokenizer(analyzerModule.analyze("premiseText", stringToMatch)).countTokens();
 
-        for (int i = 0; i < hits.length; i++) {
-            Document doc = this.searcher.doc(hits[i].doc);
-            if (new AnalyzerModule().analyze("premiseText",stringToMatch).length() == new AnalyzerModule().analyze("premiseText",doc.get("premiseText")).length()){
-                Map<String,String> match = new HashMap<>();
-                match.put("argumentId", doc.get("argumentId"));
-                match.put("premiseId", doc.get("premiseId"));
-                if(!result.contains(match)){
-                    result.add(match);
+        LinkedList<Map<String,String>> result = new LinkedList<>();
+        if (wordsCount != 0) {
+            Query query = this.parser.createPhraseQuery("premiseText", analyzerModule.analyze("premiseText", stringToMatch));
+
+            int matchNumber = this.searcher.count(query);
+            if (matchNumber == 0) {
+                return result;
+            }
+            ScoreDoc[] hits = this.searcher.search(query, matchNumber).scoreDocs;
+
+            for (int i = 0; i < hits.length; i++) {
+                Document doc = this.searcher.doc(hits[i].doc);
+                if (new AnalyzerModule().analyze("premiseText", stringToMatch).length() == new AnalyzerModule().analyze("premiseText", doc.get("premiseText")).length()) {
+                    Map<String, String> match = new HashMap<>();
+                    match.put("argumentId", doc.get("argumentId"));
+                    match.put("premiseId", doc.get("premiseId"));
+                    if (!result.contains(match)) {
+                        result.add(match);
+                    }
                 }
             }
         }
+
         return result;
     }
 
