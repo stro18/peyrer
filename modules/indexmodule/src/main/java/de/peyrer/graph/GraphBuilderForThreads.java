f720ec82ea9c39f716c9fd97a520a8039f08ff81
Stefan Rosenlund <sr54loso@studserv.uni-leipzig.de>
2020-08-28 19:39:36 +0200

    Added logging of correlation length - score

--- modules/indexmodule/src/main/java/de/peyrer/graph/GraphBuilderForThreads.java
+++ modules/indexmodule/src/main/java/de/peyrer/graph/GraphBuilderForThreads.java
@@ -16,6 +16,14 @@ public class GraphBuilderForThreads implements IGraphBuilder {
 
     private final ArgumentRepository repository;
 
+    public static int numberOfPhrasesWithLowLength;
+
+    public static double averageScoreForPhrasesWithLowLength;
+
+    public static int numberOfPhrasesWithHighLength;
+
+    public static double averageScoreForPhrasesWithHighLength;
+
     public GraphBuilderForThreads(IGraphBuilder.GraphType graphType) throws InvalidSettingValueException {
         this.repository = new ArgumentRepository();
 
@@ -80,6 +88,11 @@ public class GraphBuilderForThreads implements IGraphBuilder {
         for (Map.Entry<String, String> entry : analysis.entrySet()) {
             System.out.println(entry.getKey() + entry.getValue());
         }
+
+        System.out.println("Average score for the highest ranked matches for conclusions (or premises) with less than 5 words: "
+                + averageScoreForPhrasesWithLowLength / numberOfPhrasesWithLowLength);
+        System.out.println("Average score for the highest ranked matches for conclusions (or premises) with at least 10 words: "
+                + averageScoreForPhrasesWithHighLength / numberOfPhrasesWithHighLength);
 
         return graph;
     }
