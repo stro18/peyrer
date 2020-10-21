d9eb188d0b6607a27a5da4966b5d01ccde24025a
Stefan Rosenlund <sr54loso@studserv.uni-leipzig.de>
2020-10-21 07:18:52 +0200

    Added descriptions to helper scripts

--- helper_scripts/evaluation.sh
+++ helper_scripts/evaluation.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# script for using https://github.com/usnistgov/trec_eval to evaluate files in ../out directory
+# e.g. ./evaluation.sh -q ../../trec_eval/new.qrels -r ../out -p out-peyrer_BM25
+
 while getopts q:r:p: option
 do
 case "${option}"
